8747d2958177a688e8e1a6d2cd1c404f3f4c2e57
admon3 <31572749+admon3@users.noreply.github.com>
2023-01-28 17:49:54 -0400

    enlace a atractivos
    para actividades que hacer y eventos

--- admin_panel/src/services/SiteServices.js
+++ admin_panel/src/services/SiteServices.js
@@ -8,13 +8,16 @@ export default {
         return Api().get(`/hospedajes?filter[include]=servicios&filter[include]=tipos_habitacion`);
     },
     getAtractivoLimit(limit){
-        return Api().get(`/atractivos?filter[limit]=${limit}&filter[where][inq][categoria]=Actividades%20Que%20Hacer&filter[where][inq][categoria]=Eventos%20Programados`);
+        return Api().get(`/atractivos?filter[limit]=${limit}&filter[where][or][0][categoria]=Sitios%20Naturales&filter[where][or][1][categoria]=Etnografía%20y%20Folklore&filter&filter[where][or][2][categoria]=Patrimonio%20Urbano%20Arquitectónico&filter&filter[where][or][3][categoria]=Realizaciones%20técnicas%20científicas`);
     },
     getEventosLimit(limit){
         return Api().get(`/atractivos?filter[limit]=${limit}&filter[where][and][0][categoria]=Eventos%20Programados`);
     },
-    getActividadLimit(limit){
-        return Api().get(`/atractivos?filter[limit]=${limit}&filter[where][and][0][categoria]=Actividades%20Que%20Hacer`);
+    getActividad(){
+        return Api().get(`/atractivos?filter[where][and][0][categoria]=Actividades%20Que%20Hacer`);
+    },
+    getActividadCount(){
+        return Api().get(`/atractivos/count?where[categoria]=Actividades%20Que%20Hacer`);
     },
     getDataLimit(module, limit){
         return Api().get(`/${module}?filter[limit]=${limit}`);
